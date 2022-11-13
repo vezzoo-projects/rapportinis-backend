68fa7c4ea7b5118c8194be1e4e4da352997396f7
Luca Vezzoli <luca@implast.it>
2022-11-13 17:06:51 +0100

    Improve logs

--- src/server.js
+++ src/server.js
@@ -7,6 +7,10 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const server = express()
 
+function log(method, path, status, body) {
+    console.log(method + ' ' + path + ', status' + status + ', body' + JSON.stringify(body))
+}
+
 connect().then(async () => {
     server.use(bodyParser.json())
     server.use(cors())
@@ -14,40 +18,40 @@ connect().then(async () => {
         console.log(`Server listening at ${CONNECTION.port}`)
 
         server.post('/login', async (req, res) => {
-            console.log('POST /login')
             const result = await doLogin(req.body)
+            log('POST', '/login', result.status, result.body)
 
             res.status(result.status)
             res.send(result.body)
         })
 
         server.post('/addActivity', async (req, res) => {
-            console.log('POST /addActivity')
             const result = await addActivity(req.body, req.headers.user)
+            log('POST', '/addActivity', result.status, result.body)
 
             res.status(result.status)
             res.send(result.body)
         })
 
         server.post('/editActivity', async (req, res) => {
-            console.log('POST /editActivity')
             const result = await editActivity(req.body, req.headers.user)
+            log('POST', '/editActivity', result.status, result.body)
 
             res.status(result.status)
             res.send(result.body)
         })
 
         server.post('/getComputedActivities', async (req, res) => {
-            console.log('POST /getComputedActivities')
             const result = await calculateTimes(req.headers.user, req.body.startTimestamp)
+            log('POST', '/getComputedActivities', result.status, result.body)
 
             res.status(result.status)
             res.send(result.body)
         })
 
         server.post('/getRawActivities', async (req, res) => {
-            console.log('POST /getRawActivities')
             const result = await getRawActivities(req.headers.user, req.body.startTimestamp)
+            log('POST', '/getRawActivities', result.status, result.body)
 
             res.status(result.status)
             res.send(result.body)
